fa0903eeca4be7f19bfe0d3821ef503f5ea50040
Albjary <132553809+Albjary@users.noreply.github.com>
2024-03-10 13:08:06 +0300

    Update KAWA.py

--- KAWA.py
+++ KAWA.py
@@ -63,7 +63,7 @@ t=(mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss)
 
 hours = (now.hour)
 x = datetime.datetime.now()
-g= datetime.datetime(2025, 3, 10, 00, 00 ,0)
+g= datetime.datetime(2024, 3, 17, 00, 00 ,0)
 
 if (x.strftime("%x"))>(g.strftime("%x")):
  print('\n\n')
